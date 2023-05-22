b52e7604ea1568138ffcd9441b9e4cce3312c1c5
Virginia Butler <vwbutler@gmail.com>
2023-05-22 18:24:58 -0400

    local storage updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,32 @@ var apiKey = "0dabe66a652b4986d45a4f8a9323257e";
 var searchButton = document.getElementById("search");
 searchButton.addEventListener("click", getWeather);
 
-function getWeather() {
+function displayHistory() {
+  var history = JSON.parse(localStorage.getItem("searches")) || [];
+  document.getElementById("history").innerHTML = "";
+  history.forEach((city) => {
+    var cityEl = document.createElement("button");
+    cityEl.innerText = city;
+    cityEl.addEventListener("click", function () {
+      getWeather(null, city);
+    });
+    document.getElementById("history").appendChild(cityEl);
+  });
+}
+
+displayHistory();
+
+function getWeather(event, prevCity) {
   var cityInput = document.getElementById("city-search");
-  var city = cityInput.value;
+  var city = prevCity || cityInput.value;
+  var history = JSON.parse(localStorage.getItem("searches")) || [];
+  if (!history.includes(city)) {
+    history.push(city);
+  }
+
+  localStorage.setItem("searches", JSON.stringify(history));
+  displayHistory();
+
   getCity(city);
   getCityAgain(city);
 }
@@ -28,8 +51,9 @@ function getCity(city) {
       cityEl.innerText = data.name;
       document.getElementById("current-weather").appendChild(cityEl);
 
-      var dateEl = document.createElement("span");
-      dateEl.innerText = dayjs.unix(data.dt).format("MM-DD-YYYY");
+      var dateEl = document.createElement("h3");
+      dateEl.innerText =
+        "Today: " + dayjs.unix(data.dt).format("dddd, MMMM D, YYYY");
       document.getElementById("current-weather").appendChild(dateEl);
 
       var imageEl = document.createElement("img");
@@ -68,20 +92,34 @@ function getCityAgain(city) {
       document.getElementById("forecast-weather").innerHTML = "";
       for (var i = 7; i < data.list.length; i += 8) {
         var forecastDay = data.list[i];
-        var forecastDateEl = document.createElement("p");
+        var forecastDateEl = document.createElement("h3");
         forecastDateEl.innerText = dayjs
           .unix(forecastDay.dt)
-          .format("MM-DD-YYYY");
+          .format("dddd, MMMM D, YYYY");
+
         document.getElementById("forecast-weather").appendChild(forecastDateEl);
+
+        var forecastImageEl = document.createElement("img");
+        forecastImageEl.src =
+          "https://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png";
+        forecastImageEl.style.width = "50px";
+        document
+          .getElementById("forecast-weather")
+          .appendChild(forecastImageEl);
+
+        var forecastTempEl = document.createElement("p");
+        forecastTempEl.innerText = "Temp: " + data.list[i].main.temp;
+        document.getElementById("forecast-weather").appendChild(forecastTempEl);
+
         var forecastWindspeedEl = document.createElement("p");
         forecastWindspeedEl.innerText =
           "Wind speed: " + data.list[i].wind.speed;
         document
           .getElementById("forecast-weather")
           .appendChild(forecastWindspeedEl);
-        var forecastTempEl = document.createElement("p");
-        forecastTempEl.innerText = "Temp: " + data.list[i].main.temp;
-        document.getElementById("forecast-weather").appendChild(forecastTempEl);
+
         var forecastHumidityEl = document.createElement("p");
         forecastHumidityEl.innerText =
           "Humidity: " + data.list[i].main.humidity;
